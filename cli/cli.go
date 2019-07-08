304ab330e37ba848975969e11b0abf96359ae221
zhushen <zhushen@datagrand.com>
2019-07-08 23:57:26 +0800

    feat: cli tools

---
+++ cli/cli.go
@@ -0,0 +1,7 @@
+package main
+
+import "go-weixin/cli/cmd"
+
+func main() {
+	cmd.Execute()
+}
---
+++ cli/cmd/create_user.go
@@ -0,0 +1,39 @@
+package cmd
+
+import (
+	"errors"
+	"fmt"
+	"github.com/spf13/cobra"
+	"go-weixin/service/models"
+)
+
+var createUserCmd = &cobra.Command{
+	Use:   "createsuperuser",
+	Short: "创建管理员用户",
+	Long:  "创建管理员用户",
+	Args: func(cmd *cobra.Command, args []string) error {
+		if len(args) < 2 {
+			return errors.New("请输入正确的用户名和密码")
+		}
+		return nil
+	},
+	Run: func(cmd *cobra.Command, args []string) {
+		CreateUser(args)
+	},
+}
+
+func CreateUser(args []string) {
+	username := args[0]
+	password := args[1]
+	user := models.User{Username:username, Password:password}
+	err := models.CreateUser(user)
+	if err != nil {
+		fmt.Println(err)
+	} else {
+		fmt.Println("create user success!")
+	}
+}
+
+func init() {
+	rootCmd.AddCommand(createUserCmd)
+}
